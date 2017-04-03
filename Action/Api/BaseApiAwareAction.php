a01238a82841324f5051ba1488ada95d180ba17c
Geoffroy de Corbiac <geoffroydecorbiac@gmail.com>
2017-04-03 14:08:57 +0200

    Create empty payum skeleton

---
+++ Action/Api/BaseApiAwareAction.php
@@ -0,0 +1,20 @@
+<?php
+namespace Gontran\SyliusPayboxBundle\Action\Api;
+
+use Payum\Core\Action\ActionInterface;
+use Payum\Core\ApiAwareInterface;
+use Payum\Core\ApiAwareTrait;
+use Payum\Core\GatewayAwareInterface;
+use Payum\Core\GatewayAwareTrait;
+use Gontran\SyliusPayboxBundle\Api;
+
+abstract class BaseApiAwareAction implements ActionInterface, GatewayAwareInterface, ApiAwareInterface
+{
+    use GatewayAwareTrait;
+    use ApiAwareTrait;
+
+    public function __construct()
+    {
+        $this->apiClass = Api::class;
+    }
+}
---
+++ src/SkeletonGatewayFactory.php
@@ -0,0 +1,47 @@
+<?php
+namespace Gontran\SyliusPayboxBundle;
+
+use Gontran\SyliusPayboxBundle\Action\AuthorizeAction;
+use Gontran\SyliusPayboxBundle\Action\CancelAction;
+use Gontran\SyliusPayboxBundle\Action\ConvertPaymentAction;
+use Gontran\SyliusPayboxBundle\Action\CaptureAction;
+use Gontran\SyliusPayboxBundle\Action\NotifyAction;
+use Gontran\SyliusPayboxBundle\Action\RefundAction;
+use Gontran\SyliusPayboxBundle\Action\StatusAction;
+use Payum\Core\Bridge\Spl\ArrayObject;
+use Payum\Core\GatewayFactory;
+
+class PayboxGatewayFactory extends GatewayFactory
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function populateConfig(ArrayObject $config)
+    {
+        $config->defaults([
+            'payum.factory_name' => 'paybox',
+            'payum.factory_title' => 'Paybox',
+            'payum.action.capture' => new CaptureAction(),
+            'payum.action.authorize' => new AuthorizeAction(),
+            'payum.action.refund' => new RefundAction(),
+            'payum.action.cancel' => new CancelAction(),
+            'payum.action.notify' => new NotifyAction(),
+            'payum.action.status' => new StatusAction(),
+            'payum.action.convert_payment' => new ConvertPaymentAction(),
+        ]);
+
+        if (false == $config['payum.api']) {
+            $config['payum.default_options'] = array(
+                'sandbox' => true,
+            );
+            $config->defaults($config['payum.default_options']);
+            $config['payum.required_options'] = [];
+
+            $config['payum.api'] = function (ArrayObject $config) {
+                $config->validateNotEmpty($config['payum.required_options']);
+
+                return new Api((array) $config, $config['payum.http_client'], $config['httplug.message_factory']);
+            };
+        }
+    }
+}
